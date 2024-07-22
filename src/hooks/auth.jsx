898d57ba91d7e4ea86ae9be54b30f2d5a6d637e8
giovannivicentin <giovannifvicentin@gmail.com>
2024-07-22 14:43:49 -0300

    feat: Add authentication hooks and context

---
+++ src/hooks/auth.jsx
@@ -0,0 +1,65 @@
+import { createContext, useState, useEffect } from "react";
+import PropTypes from 'prop-types';
+import { api } from '../service/api';
+
+export const AuthContext = createContext({});
+
+function AuthProvider({ children }) {
+    const [data, setData] = useState({});
+
+    async function signIn({ email, password }) {
+        try {
+            const response = await api.post("/sessions", {email, password});
+            const { user, token } = response.data;
+
+            localStorage.setItem("@foodExplorer:user", JSON.stringify(user));
+            localStorage.setItem("@foodExplorer:token", token);
+
+            api.defaults.headers.common['Authorization'] = `Bearer ${token}`;
+
+            setData({user, token});
+        }
+        catch (error) {
+            if (error.response) {
+                alert(error.response.data.message);
+            } else {
+                alert("Não foi possível entrar.");
+            }
+        }
+    }
+
+    function signOut() {
+        localStorage.removeItem("@foodExplorer:token");
+        localStorage.removeItem("@foodExplorer:user");
+        setData({});
+    }
+
+    useEffect(() => {
+        const token = localStorage.getItem("@foodExplorer:token");
+        const user = localStorage.getItem("@foodExplorer:user");
+
+        if (token && user) {
+            api.defaults.headers.common['Authorization'] = `Bearer ${token}`;
+            setData({
+                token,
+                user: JSON.parse(user)
+            });
+        }
+    }, []);
+
+    return (
+        <AuthContext.Provider value={{
+            signIn,
+            signOut,
+            user: data.user,
+        }}>
+            {children}
+        </AuthContext.Provider>
+    );
+}
+
+AuthProvider.propTypes = {
+    children: PropTypes.node.isRequired
+};
+
+export { AuthProvider };
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,19 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import { ThemeProvider  } from 'styled-components';
+import theme from './styles/theme';
+import GlobalStyles from './styles/global';
+import { AuthProvider } from "./hooks/auth";
+import { Routes } from "./routes"
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <ThemeProvider theme={theme}>
+      <GlobalStyles />
+      <AuthProvider>
+        <Routes />
+      </AuthProvider>
+    </ThemeProvider>
+  </React.StrictMode>
 )
